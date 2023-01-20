800299bfd3d70fad1378606632e6f4e276885085
Andrew Knapick <ajknapick@yahoo.com>
2023-01-20 13:36:27 -0600

    Refactor: Add responsiveness to Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,12 +5,13 @@ import { NavLink } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Form.css";
 
-const Form = ({ triviaType, fetchCodingData, fetchGeneralData, addQuestions}) => {
+const Form = ({ triviaType, fetchCodingData, fetchGeneralData, addQuestions }) => {
 
-  const [codeOptions] = useState([ "JavaScript", "HTML", "PHP", "Laravel" ]);
-  const [generalOptions] = useState([ "Geography", "History", "Science & Nature", "Science: Computers", "Animals" ]);
+  const [codeOptions] = useState(["JavaScript", "HTML", "PHP", "Laravel"]);
+  const [generalOptions] = useState(["Geography", "History", "Science & Nature", "Science: Computers", "Animals"]);
   const [difficulty, setDifficulty] = useState("");
   const [numOfQuestions, setNumOfQuestions] = useState("");
   const [topic, setTopic] = useState("");
@@ -43,60 +44,68 @@ const Form = ({ triviaType, fetchCodingData, fetchGeneralData, addQuestions}) =>
   };
 
   return (
-    <Container fluid="md">
-      <Row>
-        <Col>
-          <section>
-            <h1 className="form-header">What Do You Know?</h1>
-            <div className="topic">
-              <h2 className="topic-title">Choose a Topic</h2>
-              <Dropdown
-                className="topic-dropdown"
-                options={
-                  triviaType === "programming"
-                    ? codeOptions
-                    : generalOptions
-                }
-                onSelect={(selection) => convertTopicToNumber(selection.value)}
-              />
-            </div>
-            <div className="difficulty">
-              <h2 className="difficulty-title">Choose a Difficulty</h2>
-              <Dropdown
-                className="difficulty-dropdown"
-                options={["Easy", "Medium", "Hard"]}
-                onSelect={(selection) => setDifficulty(selection.value)}
-              />
-            </div>
-            <div className="numQuestions">
-              <h2 className="numQuestions-title">Number of Questions</h2>
-              <Dropdown
-                className="numQuestions-dropdown"
-                options={[5, 10, 15]}
-                onSelect={(value) => setNumOfQuestions(value.value)}
-              />
-            </div>
-            <nav className="form-nav">
-              <NavLink className="trivia-selection" to="/">
-                Trivia Selection
-              </NavLink>
-              <NavLink className="saved-questions" to="/saved-questions">
-                View Saved Questions
-              </NavLink>
+    <Container fluid={true}>
+      <center>
+        <h1 className="form-header">What Do You Know?</h1>
+        <div className="form-selections">
+          <Row>
+            <Col>
+              <div className="topic" style={{marginBottom: "2rem"}}>
+                <h2 className="topic-title">Choose a Topic</h2>
+                <Dropdown
+                  className="topic-dropdown"
+                  options={
+                    triviaType === "programming"
+                      ? codeOptions
+                      : generalOptions
+                  }
+                  onSelect={(selection) => convertTopicToNumber(selection.value)}
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="difficulty" style={{marginBottom: "2rem"}}>
+                <h2 className="difficulty-title">Choose a Difficulty</h2>
+                <Dropdown
+                  className="difficulty-dropdown"
+                  options={["Easy", "Medium", "Hard"]}
+                  onSelect={(selection) => setDifficulty(selection.value)}
+                />
+              </div>
+            </Col>
+            <Col>
+              <div className="numQuestions">
+                <h2 className="numQuestions-title">Number of Questions</h2>
+                <Dropdown
+                  className="numQuestions-dropdown"
+                  options={[5, 10, 15]}
+                  onSelect={(value) => setNumOfQuestions(value.value)}
+                />
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <nav className="form-nav">
+          <Row >
+            <Col sm={1} md={4} style={{ marginBottom: "2rem" }}>
+              <NavLink className="trivia-selection" to="/">Trivia Selection</NavLink>
+            </Col>
+            <Col sm={1} md={4} style={{ marginBottom: "2rem" }}>
+              <NavLink className="saved-questions" to="/saved-questions">View Saved Questions</NavLink>
+            </Col>
+            <Col sm={1} md={4}>
               <NavLink
                 className="quiz"
                 to="/quiz"
                 onClick={() => getQuestions()}
-              >
-                Start Quiz!
-              </NavLink>
-            </nav>
-          </section>
-        </Col>
-      </Row>
+              >Start Quiz!</NavLink>
+            </Col>
+          </Row>
+        </nav>
+      </center>
     </Container>
-    );
-  }
+  );
+}
 
 export default Form;
 
